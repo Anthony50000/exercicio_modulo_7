999d488daaba5ef83ebf824ea0a8d93a0ead2c86
Anthony Gabriel de Lima Iglezias Lobo <anthony.iglezias55@gmail.com>
2023-06-23 21:14:41 -0300

    commit 2

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const  form = document.getElementById('form');
 const eValido = document.getElementById('eValido')
 const naoeValido = document.getElementById('naoEValido')
+let formEValido = false
 
+function validarFormulario() {
+    var campoA = document.getElementById("campoA").value;
+    var campoB = document.getElementById("campoB").value;
 
+    if (campoB <= campoA) {
+        alert("O número B deve ser maior que o número A!");
+    }
+}
 form.addEventListener('submit',function(e) {
     e.preventDefault();
-    const campoA = document.getElementById('campo-a');
-    const campoB = document.getElementById('campo-b');
-
-    if ((campoA.value) <= (campoB.value)) {
-        alert('eValido');
-    } else {
-        alert('nao e valido')
-    }
-}) 
+    formEValido = validarFormulario(campoA, campoB);
+})
